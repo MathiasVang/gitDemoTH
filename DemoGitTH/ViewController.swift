12a5f740a95ce00a9301b3f793237bde69e0083a
Mathias Vang Rasmussen <mathiasvangrasmussen@mikkels-imac.local>
2016-09-01 13:46:00 +0200

    Initial Commit

---
+++ DemoGitTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoGitTH
+//
+//  Created by Mathias Vang Rasmussen on 01/09/2016.
+//  Copyright © 2016 ReCapted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
